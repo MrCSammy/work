fbb2a5f54eef677cb8aa6073741ff1042c1381e1
Smart Samuel <smartsamuel017@gmail.com>
2023-03-04 00:46:37 +0100

    Committt

--- script2.js
+++ script2.js
@@ -1,3 +1,11 @@
-function PageController(){
-    let age = prompt("Before you can access this page, enter your age")
+   onload = function PageController(){
+ 
+    age = prompt("Before you can access this page, enter your age");
+    // alert(age)
+    if (age >=18) {
+        alert('You can see the site')
+    }
+    else{
+        alert("You are not welcome here because you are under age, but i'll allow you")
+    }
 }
